1dec4138d8e32d30a0cc5cc74402b58487edc644
Ritesh927 <124259042+Ritesh927@users.noreply.github.com>
2025-04-04 02:36:52 +0530

    Delete Spark2.0 directory

--- Spark2.0/src/Trying/Alphabets.java
+++
@@ -1,9 +0,0 @@
-package Trying;
-
-public class Alphabets {
-    public static void main(String[] args) {
-        for (int i = 'A'; i <= 'Z'; i++) {
-            System.out.print((char) i + " ");
-        }
-    }
-}
